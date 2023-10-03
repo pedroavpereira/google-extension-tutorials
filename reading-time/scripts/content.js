4ebbfb514b7f747b2158934de47907c2960ef0e1
pedroavpereira <pedroavpereira2000@gmail.com>
2023-10-03 23:01:03 +0100

    reading time tutorial,slightly different from tutorial

---
+++ reading-time/scripts/content.js
@@ -0,0 +1,17 @@
+const article = document.querySelector("article");
+
+if (article) {
+  const text = article.textContent;
+  const words = text.split(" ");
+  const wordCount = words.length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read`;
+
+  const heading = document.querySelector("h1");
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
